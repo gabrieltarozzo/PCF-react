e5712671fe8059f640f2013c909ad90d2d320dba
gabrieltarozzo <gabriel.tarozzo@hotmail.com>
2024-09-26 18:00:58 -0300

    alteracoes

--- Link/src/data/chartOptions.ts
+++ Link/src/data/chartOptions.ts
@@ -5,11 +5,19 @@ export const options = {
   },
   colors: ['#578ACA', '#A6C661', '#0000ff', '#008000'], // Adicionando cores
   series: {
-    0: { type: 'bars' },
-    1: { type: 'bars' },
-    2: { type: 'line', lineWidth: 2 }, // Configurando linhas para usar o segundo eixo y
-    3: { type: 'line', lineWidth: 2 }
+    0: { type: 'bars' }, // Consumo de Energia
+    1: { type: 'bars' }, // Produção de Energia
+    2: { type: 'line', lineWidth: 2 }, // Média Consumo
+    3: { type: 'line', lineWidth: 2 }  // Média Produção
   },
   bar: { groupWidth: '80%' }, // Ajuste o valor para aumentar ou diminuir o espaçamento entre as barras
   legend: { position: 'top' }, // Posicionando a legenda acima do gráfico
+  vAxes: {
+    0: { 
+      viewWindow: { min: 0 } // Eixo y para as barras começando em 0
+    },
+    1: { 
+      viewWindow: { min: 0 } // Eixo y secundário para as linhas também começando em 0
+    }
+  }
 };
